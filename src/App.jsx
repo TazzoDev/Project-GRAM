35a089e4eb7098143e87cb1a9a2504bf7d61d394
TazzoDev <141327712+TazzoDev@users.noreply.github.com>
2023-12-21 18:15:45 +0000

    added send button

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function App() {
           },
           {
             recieved: false,
-            text: "Comign right up."
+            text: "Comign right up. AAAAAAA"
           }
         ]
       },
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,21 +1,31 @@
-import { useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import Message from "./Message"
+import ArrowLeft from "../assets/icons/ArrowLeft"
+import SendIcon from "../assets/icons/SendIcon"
 
 export default function Chat(){
 
     const {state} = useLocation()
 
-    console.log(state)
 
     const messageElements = state.messages?.map((message, index) => <Message data={message} key={index}/>)
 
     return(
         <div className="chat">
             <div className="chat--header">
+                <Link to=".."><ArrowLeft /></Link>
                 <img src={"https://placehold.co/80"}/>
                 <h3>{state.name}</h3>
             </div>
-            {messageElements}
+            <section className="chat-container">
+                {messageElements}
+            </section>
+            <div className="chat--footer">
+                <div className="chat--footer--input"/>
+                <div className="chat--footer--button">
+                    <SendIcon color="white" />
+                </div>
+            </div>
         </div>
     )
 }
